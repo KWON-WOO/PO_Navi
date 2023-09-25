7d156f87562a94c71c789f0a9bf8dddc85aa0c0d
KWON-WOO <ckw075@naver.com>
2023-09-26 00:49:21 +0900

    Floating a TMap  -add TMap SDK  -set to code sdk on activity_main.xml and MainActivity.kt

--- app/src/main/java/com/example/po_navigation/MainActivity.kt
+++ app/src/main/java/com/example/po_navigation/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.po_navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import com.skt.Tmap.TMapView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val linearLayoutTmap = findViewById<View>(R.id.linearLayoutTmap) as LinearLayout
+        val tMapView = TMapView(this)
+
+        tMapView.setSKTMapApiKey(getString(R.string.app_key1))
+        linearLayoutTmap.addView(tMapView)
     }
 }
